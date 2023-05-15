9904def7980081aad2683009c2fb65f323e45136
Weizmtl <eric.zmtl@gmail.com>
2023-05-15 16:49:13 -0400

    update ArticleListItem.vue style.

--- front-end/src/views/forum/ArticleListItem.vue
+++ front-end/src/views/forum/ArticleListItem.vue
@@ -4,33 +4,36 @@
             <div class="article-body">
                 <div class="user-info">
                     <Avatar :userId="data.userId" :width="30"></Avatar>
-                    <router-link to="'/users/' + data.userId">{{ data.nickName }}</router-link>
+                    <router-link to="'/users/' + data.userId" class="link-info">{{ data.nickName }}</router-link>
                     <el-divider direction="vertical"></el-divider>
                     <div class="post-time">{{ data.postTime }}</div>
-                    <div class="address">{{ data.userIpAddress }}</div>
+                    <div class="address">&nbsp;·&nbsp;{{ data.userIpAddress }}</div>
                     <el-divider direction="vertical"></el-divider>
-                    <router-link :to="'/'">{{ data.pBoardName }}</router-link>
+                    <router-link :to="'/'" class="link-info">{{ data.pBoardName }}</router-link>
                     <template v-if="data.boardName">
-                        <el-divider direction="vertical"></el-divider>
-                        <router-link :to="'/'">{{ data.boardName }}</router-link>
+                        <span>&nbsp;/&nbsp; </span>
+                        <router-link :to="'/'" class="link-info">{{ data.boardName }}</router-link>
                     </template>
                 </div>
-                <router-link to="/">{{data.title}}</router-link>
-                <div class="summary">{{data.summary}}</div>
+                <router-link to="/" class="title">{{ data.title }}</router-link>
+                <div class="summary">{{ data.summary }}</div>
                 <div class="article-info">
                     <span class="iconfont icon-eye-solid">
-                        {{data.readCount==0 ? "Read" : data.readCount}}
+                        {{ data.readCount == 0 ? "Read" : data.readCount }}
                     </span>
 
                     <span class="iconfont icon-good">
-                        {{data.goodCount==0 ? "Like" : data.goodCount}}
+                        {{ data.goodCount == 0 ? "Like" : data.goodCount }}
                     </span>
 
                     <span class="iconfont icon-comment">
-                        {{data.commentCount==0 ? "Comment" : data.commentCount}}
+                        {{ data.commentCount == 0 ? "Comment" : data.commentCount }}
                     </span>
 
                 </div>
+            </div>
+            <div class="cover" v-if="data.cover">
+                <img :src="'/api/file/getImage/' + data.cover"/>
             </div>
         </div>
     </div>
@@ -48,12 +51,70 @@ const props = defineProps({
 
 <style lang="scss">
 .article-item {
-  .article-item-inner {
-    .article-body {
-      .user-info {
+    padding: 5px 10px 0 10px;
+
+    .article-item-inner {
+        border-bottom: 1px solid #ddd;
+        padding: 10px;
         display: flex;
-      }
+        .article-body {
+            flex:1;
+            .user-info {
+                display: flex;
+                align-items: center;
+                font-size: 14px;
+                color: #4e5969;
+
+                .link-info {
+                    margin-left: 5px;
+                    color: #494949;
+                    text-decoration: none;
+                }
+
+                .link-info:hover {
+                    color: var(--link)
+                }
+
+                .post-time {
+                    font-size: 14px;
+                    color: #9a9a9a;
+                }
+            }
+            .title{
+                font-weight:bold;
+                text-decoration: none;
+                color:#4a4a4a;
+                font-size: 16px;
+                margin: 10px 0px;
+                display: inline-block;
+            }
+            .summary{
+                font-size: 14px;
+                color:#86909c;
+            }
+            .article-info{
+                margin-top: 10px;
+                display:flex;
+                align-items: center;
+                font-size: 12px;
+                .iconfont {
+                    color: #86909c;
+                    margin-right: 25px;
+                    font-size: 14px;
+                }
+                .iconfont:before {
+                    padding-right: 3px;
+                }
+                .edit-btn {
+                    color: var(--link);
+                    cursor: pointer;
+                }
+            }
+        }
     }
-  }
+}
+
+.article-item:hover {
+    background: #fffbfb;
 }
 </style>
