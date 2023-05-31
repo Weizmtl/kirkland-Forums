e40ac4e9e50ffc5827c1d2317e621f92abda9080
Weizmtl <eric.zmtl@gmail.com>
2023-05-31 09:05:06 -0400

    update personal center avatar

--- front-end/src/views/ucenter/Ucenter.vue
+++ front-end/src/views/ucenter/Ucenter.vue
@@ -19,6 +19,17 @@
             >
               Modify Profile
             </div>
+            <div class="avatar-inner">
+              <Avatar :userId="userInfo.userId" :width="120"></Avatar>
+            </div>
+            <div class="nick-name">
+              <span>{{ userInfo.nickName }}</span>
+              <span v-if="userInfo.sex == 0" class="iconfont icon-woman"></span>
+              <span v-if="userInfo.sex == 1" class="iconfont icon-man"></span>
+            </div>
+            <div class="desc">
+              {{ userInfo.personDescription }}
+            </div>
           </div>
         </div>
       </div>
@@ -32,6 +43,32 @@ import { useRouter, useRoute } from "vue-router";
 const { proxy } = getCurrentInstance();
 const router = useRouter();
 const route = useRoute();
+
+const api = {
+  getUserInfo: "/ucenter/getUserInfo",
+  loadUserArticle: "/ucenter/loadUserArticle",
+};
+
+const userId = ref(null);
+const userInfo = ref({});
+
+const loadUserInfo = async () => {
+  let result = await proxy.Request({
+    url: api.getUserInfo,
+    params: {
+      userId: userId.value,
+    },
+    errorCallback: () => {
+      setTimeout(() => {
+        router.push("/");
+      }, 1500);
+    },
+  });
+  if (!result) {
+    return;
+  }
+  userInfo.value = result.data;
+};
 </script>
 
 <style lang="scss" scoped>
@@ -41,6 +78,30 @@ const route = useRoute();
     line-height: 35px;
     .icon-right {
       padding: 0px 5px;
+    }
+  }
+  .ucenter-panel {
+    display: flex;
+
+    .user-side {
+      width: 300px;
+      margin-right: 10px;
+
+      .avatar-panel {
+        background: #fff;
+        text-align: center;
+        padding: 10px;
+
+        .edit-btn {
+          text-align: right;
+          font-size: 14px;
+        }
+
+        .avatar-inner {
+          display: flex;
+          justify-content: center;
+        }
+      }
     }
   }
 }
