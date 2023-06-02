3dab1d78b01e88150bb8ef899b877b3298ce7189
Weizmtl <eric.zmtl@gmail.com>
2023-06-02 08:39:54 -0400

    start integral feature

---
+++ front-end/src/views/ucenter/UserIntegralRecord.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <Dialog
+      :show="dialogConfig.show"
+      :title="dialogConfig.title"
+      :buttons="dialogConfig.buttons"
+      width="600px"
+      :showCancel="false"
+      @close="dialogConfig.show = false"
+  >
+  </Dialog>
+</template>
+
+<script setup>
+
+</script>
+<style scoped lang="scss">
+
+</style>
