03ab0005abbed90a88109c26b802518f7ec1e411
zhuwei <eric.zmtl@gmail.com>
2023-06-12 22:20:32 +0800

    completed login logic

--- front-admin/src/main.js
+++ front-admin/src/main.js
@@ -3,43 +3,44 @@ import App from './App.vue'
 import router from './router'
 
 import VueCookies from 'vue-cookies'
-
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import '@/assets/base.scss'
+
+import './assets/base.scss'
 import '@/assets/icon/iconfont.css'
 
-import Request from '@/utils/Request';
-import Message from '@/utils/Message'
-import Confirm from '@/utils/Confirm'
+//global methods
 import Verify from '@/utils/Verify'
-import Utils from '@/utils/Utils'
-
-import Cover from '@/components/Cover.vue'
-import Avatar from '@/components/Avatar.vue'
-import Dialog from '@/components/Dialog.vue'
+import Message from '@/utils/Message'
+import Request from '@/utils/Request'
+import Utils from './utils/Utils'
+import Confirm from './utils/Confirm'
+// global components
+import Dialog from "@/components/Dialog.vue"
+import Avatar from "@/components/Avatar.vue"
+import Cover from "@/components/Cover.vue"
 
 
 const app = createApp(App)
+
 app.use(router)
 app.use(ElementPlus);
-
-app.config.globalProperties.Request = Request;
-app.config.globalProperties.Message = Message;
-app.config.globalProperties.Confirm = Confirm;
-app.config.globalProperties.Verify = Verify;
-app.config.globalProperties.Utils = Utils;
-
 app.config.globalProperties.VueCookies = VueCookies;
 app.config.globalProperties.globalInfo = {
-    imageUrl: "/api/file/getImage/",
-    avatarUrl: "/api/file/getAvatar/",
-    webDomain: import.meta.env.VITE_WEB_DOMAIN
+    bodyWidth: 1300,
+    avatarUrl:"/api/file/getAvatar/",
+    imageUrl:"/api/file/getImage/",
 }
+app.config.globalProperties.Verify = Verify;
+app.config.globalProperties.Message = Message;
+app.config.globalProperties.Request = Request;
+app.config.globalProperties.Utils = Utils;
+app.config.globalProperties.Confirm = Confirm;
 
-app.component("Cover", Cover);
-app.component("Avatar", Avatar);
 app.component("Dialog", Dialog);
+app.component("Avatar", Avatar);
+app.component("Cover", Cover);
+
 
 app.mount('#app')
 
--- front-admin/src/views/Login.vue
+++ front-admin/src/views/Login.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="login-body">
     <div class="login-panel">
+      <div class="login-title">Admin Login</div>
       <el-form :model="formData" :rules="rules" ref="formDataRef">
         <el-form-item prop="account">
           <el-input
-              placeholder="Please input username"
+              placeholder="Please input your account"
               v-model="formData.account"
               size="large"
           >
@@ -28,11 +29,10 @@
         <el-form-item prop="checkCode">
           <div class="check-code-panel">
             <el-input
-                placeholder="请输入验证码"
+                placeholder="Please enter your check code"
                 v-model="formData.checkCode"
                 class="input-panel"
                 size="large"
-                @keyup.enter.native="login"
             >
               <template #prefix>
                 <span class="iconfont icon-checkcode"></span>
@@ -47,34 +47,81 @@
         </el-form-item>
         <el-form-item>
           <el-button type="primary" :style="{ width: '100%' }" @click="login"
-          >Login</el-button>
+          >Login</el-button
+          >
         </el-form-item>
       </el-form>
     </div>
   </div>
 </template>
 
-<script>
-
+<script setup>
+import md5 from "js-md5";
 import { getCurrentInstance, reactive, ref } from "vue";
-import { useRouter, useRoute} from "vue-router";
+import { useRouter } from "vue-router";
 
 const { proxy } = getCurrentInstance();
 const router = useRouter();
-const route = useRoute();
-
-
-//relation with forms
-const formDataRef = ref();
-const formData = reactive({});
-const rules = {
-  title:[{required: true,message: "please enter text here"}],
+const api = {
+  checkCode: "api/checkCode",
+  login: "/login",
 };
 
 const checkCodeUrl = ref(api.checkCode);
+const changeCheckCode = () => {
+  checkCodeUrl.value = api.checkCode + "?" + new Date().getTime();
+};
 
+//form
+const formDataRef = ref(null);
+const formData = reactive({});
+
+const rules = {
+  account: [
+    {
+      required: true,
+      message: "Please input username",
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "Please input password",
+    },
+  ],
+  checkCode: [
+    {
+      required: true,
+      message: "Please input check code",
+    },
+  ],
+};
+
+const login = () => {
+  formDataRef.value.validate(async (valid) => {
+    if (!valid) {
+      return;
+    }
+    let params = Object.assign({}, formData);
+    params.password = md5(params.password);
+    let result = await proxy.Request({
+      url: api.login,
+      params: params,
+      errorCallback: () => {
+        changeCheckCode();
+      },
+    });
+    if (!result) {
+      return;
+    }
+    proxy.Message.success("Login successful", () => {
+      router.push("/");
+    });
+    proxy.VueCookies.set("userInfo", result.data, 0);
+  });
+};
 </script>
 
-<style >
+<style lang="scss">
 
 </style>
