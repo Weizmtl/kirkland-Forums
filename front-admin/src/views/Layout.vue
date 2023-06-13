b8b8f0895ba20a3720d0c2f7c619c6f70307f5e2
zhuwei <eric.zmtl@gmail.com>
2023-06-14 07:05:04 +0800

    update right side display

--- front-admin/src/views/Layout.vue
+++ front-admin/src/views/Layout.vue
@@ -2,47 +2,88 @@
   <div class="layout-body">
     <el-container>
       <el-aside class="aside" :style="{ width: asideWidth + 'px' }">
-      </el-aside>
-      <<div class="logo">
-      <span v-if="!menuCollapse">KKbbs back-end Management</span>
-    </div>
-      <div class="menu-panel">
-        <el-menu
-            :default-openeds="defaultOpeneds"
-            :collapse-transition="false"
-            :collapse="menuCollapse"
-            class="el-menu-vertical-demo"
-            default-active="2"
-            background-color="#3d3c4a"
-            text-color="#fff"
-            active-text-color="#fff"
-            router
-            :defaultActive="defaultActive"
-        >
-          <template v-for="item in menuList">
-            <el-sub-menu :index="item.path" v-if="item.children">
-              <template #title>
-                <i :class="['iconfont', item.icon]"></i>
-                <span class="menu-name">{{ item.menuName }}</span>
-              </template>
-              <el-menu-item
-                  :index="subItem.path"
-                  v-for="subItem in item.children"
-              >
+        <div class="logo">
+          <span v-if="!menuCollapse">KKbbs Management</span>
+        </div>
+        <div class="menu-panel">
+          <el-menu
+              :default-openeds="defaultOpeneds"
+              :collapse-transition="false"
+              :collapse="menuCollapse"
+              class="el-menu-vertical-demo"
+              default-active="2"
+              background-color="#3d3c4a"
+              text-color="#fff"
+              active-text-color="#fff"
+              router
+              :defaultActive="defaultActive"
+          >
+            <template v-for="item in menuList">
+              <el-sub-menu :index="item.path" v-if="item.children">
+                <template #title>
+                  <i :class="['iconfont', item.icon]"></i>
+                  <span class="menu-name">{{ item.menuName }}</span>
+                </template>
+                <el-menu-item
+                    :index="subItem.path"
+                    v-for="subItem in item.children"
+                >
                   <span class="menu-name">{{
                       subItem.menuName
                     }}</span></el-menu-item
-              >
-            </el-sub-menu>
-            <el-menu-item :index="item.path" v-else>
-              <i :class="['iconfont', item.icon]"></i>
-              <template #title>
-                <span class="menu-name">{{ item.menuName }}</span>
+                >
+              </el-sub-menu>
+              <el-menu-item :index="item.path" v-else>
+                <i :class="['iconfont', item.icon]"></i>
+                <template #title>
+                  <span class="menu-name">{{ item.menuName }}</span>
+                </template>
+              </el-menu-item>
+            </template>
+          </el-menu>
+        </div>
+      </el-aside>
+      <el-container>
+        <el-header class="header">
+          <div
+              :class="[
+              'op-menu',
+              'iconfont',
+              menuCollapse ? 'icon-expand' : 'icon-collapse',
+            ]"
+              @click="opMenu"
+          ></div>
+          <div class="menu-bread">
+            <el-breadcrumb>
+              <template v-for="item in menuBreadCrumbList">
+                <el-breadcrumb-item v-if="item.name">
+                  {{ item.name }}
+                </el-breadcrumb-item>
               </template>
-            </el-menu-item>
-          </template>
-        </el-menu>
-      </div>
+            </el-breadcrumb>
+          </div>
+        </el-header>
+        <el-main class="main-content">
+          <div class="tag-content">
+            <el-tabs
+                type="border-card"
+                v-model="defaultActive"
+                @tab-change="tabClick"
+                @edit="editTab"
+            >
+              <el-tab-pane
+                  :name="item.path"
+                  :label="item.menuName"
+                  :closable="tabList.length > 1"
+                  v-for="item in tabList"
+              ></el-tab-pane>
+            </el-tabs>
+          </div>
+          <div class="content-body">
+            <router-view />
+          </div>
+        </el-main>
+      </el-container>
     </el-container>
   </div>
 </template>
@@ -106,6 +147,7 @@ const menuList = [
     ],
   },
 ];
+
 </script>
 
 <style lang="scss">
