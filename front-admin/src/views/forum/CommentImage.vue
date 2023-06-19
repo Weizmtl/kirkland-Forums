5dd7bf344a00056f423242088c35b2fcf8001300
zhuwei <eric.zmtl@gmail.com>
2023-06-20 06:26:21 +0800

    update CommentImage component

---
+++ front-admin/src/views/forum/CommentImage.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-image
+      class="comment-img"
+      :src="proxy.globalInfo.imageUrl + src"
+      :zoom-rate="1.2"
+      :preview-src-list="imgList ? imgList : [src]"
+      :initial-index="0"
+      fit="scale-down"
+      loading="lazy"
+      hide-on-click-modal
+  />
+</template>
+
+<script>
+export default {
+  name: "CommentImage"
+}
+</script>
+
+<style scoped>
+
+</style>
