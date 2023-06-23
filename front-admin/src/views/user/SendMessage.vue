9093f66f1864fb82f44466be08f84e306f7f6758
zhuwei <eric.zmtl@gmail.com>
2023-06-23 20:57:05 +0800

    update send message component

--- front-admin/src/views/user/SendMessage.vue
+++ front-admin/src/views/user/SendMessage.vue
@@ -1,5 +1,41 @@
 <template>
+  <div>
+    <Dialog
+        :show="dialogConfig.show"
+        :title="dialogConfig.title"
+        :buttons="dialogConfig.buttons"
+        width="500px"
+        @close="dialogConfig.show = false"
+    >
+      <el-form
+          :model="formData"
+          :rules="rules"
+          ref="formDataRef"
+          label-width="80px"
+      >
+        <el-form-item label="Message content" prop="message">
+          <el-input
+              clearable
+              placeholder="Please enter a message"
+              type="textarea"
+              :rows="5"
+              :maxlength="200"
+              resize="none"
+              show-word-limit
+              v-model="formData.message"
+          ></el-input>
+        </el-form-item>
 
+        <el-form-item label="credit" prop="integral">
+          <el-input
+              clearable
+              placeholder="The integral, it's empty or zero doesn't add or subtract, it can be negative"
+              v-model="formData.integral"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+    </Dialog>
+  </div>
 </template>
 
 <script>
