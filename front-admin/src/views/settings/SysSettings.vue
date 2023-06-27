ba6afcfa2ae237bf666fbc081a34837c6b48444a
zhuwei <eric.zmtl@gmail.com>
2023-06-27 21:48:11 +0800

    update system setting template(mail setting)

--- front-admin/src/views/settings/SysSettings.vue
+++ front-admin/src/views/settings/SysSettings.vue
@@ -20,6 +20,30 @@
             </el-form-item>
           </el-col>
         </el-row>
+
+        <el-divider content-position="left">mail setting</el-divider>
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="mail title" prop="emailTitle">
+              <el-input
+                  v-model="formData.emailTitle"
+                  placeholder="please input title"
+                  maxlength="200"
+              >
+              </el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="mail content" prop="emailContent">
+              <el-input
+                  v-model="formData.emailContent"
+                  placeholder="Please enter the message content, the verification code use %s placeholder"
+                  maxlength="300"
+              >
+              </el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
       </el-form>
     </div>
 </template>
