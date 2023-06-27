41f4b50b6f797e3d4705887781710665db46b6af
zhuwei <eric.zmtl@gmail.com>
2023-06-27 21:16:19 +0800

    update system setting template

--- front-admin/src/views/settings/SysSettings.vue
+++ front-admin/src/views/settings/SysSettings.vue
@@ -1,5 +1,27 @@
 <template>
-<div>system settings</div>
+    <div>
+      <el-form
+          :model="formData"
+          :rules="rules"
+          ref="formDataRef"
+          label-width="150px"
+          :style="{ width: '800px' }"
+      >
+        <el-divider content-position="left">registration message</el-divider>
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="welcome" prop="registerWelcomInfo">
+              <el-input
+                  v-model="formData.registerWelcomInfo"
+                  placeholder="please input welcome message"
+                  maxlength="250"
+              >
+              </el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
 </template>
 
 <script>
